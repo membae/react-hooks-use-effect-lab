9e426ae2d027e5440f41250f5b1fd2db6076d042
membae <membaphilip1@gmail.com>
2024-08-07 19:40:20 +0300

    last one not working

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime === 1) {
+          clearInterval(timer);
+          onAnswered(false);
+          return 10; // Reset the timer to 10 seconds for the next question
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(timer);
+  }, [onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
